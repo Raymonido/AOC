8b3f4b706a68cd9efad6adb624cb1582f8a063b4
Ramon Hufschmid <ramonhufschmid@blkb.ch>
2024-12-12 15:54:05 +0100

    Day2pt1

--- Day2pt1/src/Main.java
+++ Day2pt1/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       File file = new File("/home/f04089/Projects/AOC/Day2pt1/input/input.txt");
+       File file = new File("/home/f04089/Projects/AOC/AOC/Day2pt1/input/input.txt");
         Scanner sc = new Scanner(file);
         List<List<Integer>> lineNumbers = new ArrayList<List<Integer>>();
         List<String> lines = new ArrayList<String>();
@@ -21,15 +21,31 @@ public class Main {
             lineNumbers.add(row);
         }
         int safe = 0;
-        for (int i = 0; i < lineNumbers.size(); i++) {
-            for (int j = 0; j < lineNumbers.get(i).size(); j++) {
-                if (lineNumbers.get(i).get(j) < lineNumbers.get(i).get(j += 1) && lineNumbers.get(i).get(j) - lineNumbers.get(i).get(j += 1) <= 3) {
-                    System.out.println(lineNumbers.get(i));
-                    safe++;
-                }
-
+        for (List<Integer> lineNumber : lineNumbers) {
+            if (isValidLine(lineNumber)) {
+                safe++;
+            } else {
+                System.out.println(lineNumber + "Ist nicht gut!");
             }
         }
+
         System.out.println(safe);
+    }
+    public static boolean isValidLine(List<Integer> line) {
+        boolean isDescending = true;
+        boolean isAscending = true ;
+        for (int i = 0; i < line.size()-1; i++) {
+            int diff = line.get(i + 1) - line.get(i);
+            if (Math.abs(diff) > 3 || diff == 0) {
+                return false;
+            }
+            if (diff > 0) {
+                isDescending = false;
+            }
+            if (diff < 0) {
+                isAscending = false;
+            }
+        }
+        return isAscending || isDescending;
     }
 }
